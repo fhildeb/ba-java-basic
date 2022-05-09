8cabda7081d2e3155dbb2d9ab26cf152afbc042d
fhildeb <felix@lukso.io>
2022-05-09 23:04:24 +0200

    adding project 3

---
+++ java-practical-03/src/Rechteck/Punkt.java
@@ -0,0 +1,89 @@
+package Rechteck;
+
+public class Punkt
+{
+	// Deklarieren der Variablen
+	private double x;
+	private double y;
+	
+	// Klassen-Konstruktor mit Parametern
+	public Punkt(double xWert, double yWert)
+	{
+		this.x = xWert;
+		this.y = yWert;
+	}
+	
+	// Klassen-Konstruktor ohne Parameter
+	public Punkt()
+	{
+		this.x = 0;
+		this.y = 0;
+	}
+	
+	// Auslesefunktion X-Wert
+	public double getx()
+	{
+		return this.x;
+	}
+	
+	//Auslesefunktion Y-Wert
+	public double gety()
+	{
+		return this.y;
+	}
+	
+	// Funktion zum Versetzen eines Punktes
+	public void versetzen(double xNeu, double yNeu)
+	{
+		this.x = xNeu;
+		this.y = yNeu;
+	}
+	
+	// Funktion zum Verschieben eines Punktes
+	public void verschieben(double dx, double dy)
+	{
+		this.x = this.x + dx;
+		this.y = this.y + dy;
+	}
+	
+	// Ausgabe des Objektes
+	public String toString()
+	{
+		return "(" + this.x + "|" + this.y + ")";
+	}
+	
+	// Vergleich zweier Objekte
+	public boolean equals(Punkt p)
+	{
+		return ((this.getx() == p.getx()) && (this.gety() == p.gety()));
+	}
+	
+	/*
+	/ Zusatz
+   */
+   
+   //Spiegeln eines Punktes
+   public void spiegeln()
+   {
+	   this.x = -(this.x);
+	   this.y = -(this.y);
+   }
+   
+	//Abstand zum Koordinatenursprung berechnen
+	public double abstandUrsprung()
+	{
+		return Math.sqrt( ((this.x) * (this.x)) + ((this.y) * (this.y)) );
+	}
+	
+	//Winkel zur X-Achse berechnen
+	public String winkelXAchse()
+	{
+		//Cosinus berechnen
+		double cos = ((this.x) / (this.abstandUrsprung()));
+		//Radians berechnen
+		double acos = Math.acos( cos );
+		//Grad berechnen und ausgeben
+		return Math.toDegrees( acos ) + " Grad";
+	}
+   
+}
---
+++ java-practical-03/src/Rechteck/Rechteck.java
@@ -0,0 +1,142 @@
+package Rechteck;
+
+public class Rechteck
+{
+	//Deklarieren der Variablen
+	private Punkt startpunkt;
+	private double hoehe;
+	private double breite;
+	
+	//Konstrucktor ohne Parameter
+	public Rechteck()
+	{
+		this.startpunkt = new Punkt(0, 0);
+		this.hoehe = 0;
+		this.breite = 0;
+	}
+	//Konstruktor mit 3 Parametern
+	public Rechteck( Punkt start, double h, double b)
+	{
+		this.startpunkt = start;
+		this.hoehe = h;
+		this.breite = b;
+	}
+	
+	//Konstruktor mit 4 Parametern
+	public Rechteck(double xstart, double ystart, double h, double b)
+	{
+		this.startpunkt = new Punkt(xstart, ystart);
+		this.hoehe = h;
+		this.breite = b;
+	}
+	
+	//Ausgabe Wert Startpunkt
+	public Punkt getStartpunkt()
+	{
+		return this.startpunkt;
+	}
+	
+	//Ausgabe Wert Hoehe
+	public double getHoehe()
+	{
+		return this.hoehe;
+	}
+	
+	//Ausgabe Wert Breite
+	public double getBreite()
+	{
+		return this.breite;
+	}
+	
+	//Rechteck Flaeche berechnen
+	public double berechneFlaeche()
+	{
+		return (this.breite * this.hoehe);
+	}
+	
+	//Rechteck Umfang berechnen
+	public double berechneUmfang()
+	{
+		return (2 * (this.breite + this.hoehe));
+	}
+	
+	//Rechteck versetzen
+	public void versetzen(double xNeu, double yNeu)
+	{
+		this.startpunkt.versetzen(xNeu, yNeu);
+	}
+	
+	//Rechteck verschieben
+	public void verschieben(double dx, double dy)
+	{
+		this.startpunkt.verschieben(dx, dy);
+	}
+	
+	//String Ausgabe
+	public String toString()
+	{
+		return 	"Startpunkt: " + this.startpunkt.toString() + 
+				", Hoehe: " + this.getHoehe() + 
+				", Breite: " + this.getBreite();
+	}
+	
+	//Vergleich Rechteck
+	public boolean equals(Rechteck r)
+	{
+		return 	(
+				(this.startpunkt.equals(r.startpunkt)) && 
+				(this.hoehe == r.hoehe) &&
+				(this.breite == r.breite)
+				);
+	}
+	
+	/*
+	/ Zusatz
+   */
+   
+	//Eckpunkte des Rechtecks berechnen und ausgeben
+	public String eckpunkte()
+	{
+		Punkt alphaA = new Punkt((this.startpunkt.getx()), (this.startpunkt.gety()));
+		Punkt betaA = new Punkt((this.startpunkt.getx()), (this.startpunkt.gety() + this.breite));
+		Punkt gammaA = new Punkt((this.startpunkt.getx() + this.hoehe), (this.startpunkt.gety() + this.breite));
+		Punkt deltaA = new Punkt((this.startpunkt.getx() + this.hoehe), (this.startpunkt.gety()));
+		
+		return 	"Alpha: " + alphaA.toString() + 
+				", Beta: " + betaA.toString() + 
+				", Gamma: " + gammaA.toString() +
+				", Delta: " + deltaA.toString();
+	}
+	
+	//Rechteck spiegeln
+	public void spiegeln()
+	{
+		this.startpunkt.spiegeln();
+		startpunkt.versetzen((this.startpunkt.getx() - this.breite), (this.startpunkt.gety() - this.hoehe));
+	}
+	
+	//Auf gemeinsame Punkte vergleichen
+	public boolean[] gemeinsamePunkte(Rechteck r)
+	{
+		//Referenzpunkte anlegen
+		Punkt alphaB = new Punkt((r.getStartpunkt().getx()), (r.getStartpunkt().gety()));
+		Punkt betaB = new Punkt((r.getStartpunkt().getx()), (r.getStartpunkt().gety() + r.breite));
+		Punkt gammaB = new Punkt((r.getStartpunkt().getx() + r.hoehe), (r.getStartpunkt().gety() + r.breite));
+		Punkt deltaB = new Punkt((r.getStartpunkt().getx()) + r.hoehe, (r.getStartpunkt().gety()));
+		
+		Punkt alphaA = new Punkt((this.startpunkt.getx()), (this.startpunkt.gety()));
+		Punkt betaA = new Punkt((this.startpunkt.getx()), (this.startpunkt.gety() + this.breite));
+		Punkt gammaA = new Punkt((this.startpunkt.getx() + this.hoehe), (this.startpunkt.gety() + this.breite));
+		Punkt deltaA = new Punkt((this.startpunkt.getx() + this.hoehe), (this.startpunkt.gety()));
+		
+		//Punkte der Rechtecke vergleichen
+		boolean alphaGleich = alphaA.equals(alphaB);
+		boolean betaGleich = betaA.equals(betaB);
+		boolean gammaGleich = gammaA.equals(gammaB);
+		boolean deltaGleich = deltaA.equals(deltaB);
+
+		return new boolean[] {alphaGleich, betaGleich, gammaGleich, deltaGleich};		
+		
+	}
+	
+}
