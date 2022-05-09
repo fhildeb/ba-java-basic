76ac8c9dd135098e7ad0a4cd0573cb1005ce4b7e
fhildeb <felix@lukso.io>
2022-05-09 23:08:00 +0200

    adding project 11

---
+++ java-practical-11/src/RateSpiel/Keyboard.java
@@ -0,0 +1,102 @@
+package RateSpiel;
+
+import java.io.*;
+
+/**
+ *  Einfache Klasse zur <B>Tastatur-Eingabe</B> von <B>Zeichenketten</B> und 
+ *  <B>Zahlenwerten</B> . <BR>
+ *  Beim Einlesen von Zahlenwerten werden Format-Ausnahmen ausgeloest, wenn 
+ *  keine gueltige Zahl eingegeben wird. 
+ *
+ */
+
+public class Keyboard
+{
+    /**
+     *  Zum Einlesen der Tastatur-Eingabe wird ein <TT>BufferedReader</TT> 
+     *  vom Standard-Eingabe-Kanal verwendet. 
+     */
+    private static BufferedReader in = new BufferedReader(
+            new InputStreamReader(System.in));
+
+    /**
+     *  Lesen einer Zeichenkette von der Tastatur. Eine hier nur theoretisch 
+     *  moegliche <TT>IOException</TT> wird ohne Reaktion abgefangen. 
+     *
+     * @return    eingegebene Zeichenkette 
+     */
+    public static String readString()
+    {
+        String s = "";
+        try
+        {
+            s = in.readLine();
+        }
+        catch (IOException e)
+        {
+            /*
+             * keine Reaktion
+             */
+        }
+        ;
+        return s;
+    }
+
+    /**
+     *  Lesen eines <TT>int</TT> -Werts von der Tastatur. 
+     *
+     * @return                            eingegebener <TT>int</TT> -Wert 
+     * @exception  NumberFormatException  falls kein gueltiger <TT>int</TT> 
+     *      -Wert eingegeben wird 
+     */
+    public static int readint() throws NumberFormatException
+    {
+        String s = readString();
+        int n = Integer.parseInt(s);
+        return n;
+    }
+
+    /**
+     *  Lesen eines <TT>long</TT> -Werts von der Tastatur. 
+     *
+     * @return                            eingegebener <TT>long</TT> -Wert 
+     * @exception  NumberFormatException  falls kein gueltiger <TT>long</TT> 
+     *      -Wert eingegeben wird 
+     */
+    public static long readlong() throws NumberFormatException
+    {
+        String s = readString();
+        long n = Long.parseLong(s);
+        return n;
+    }
+
+    /**
+     *  Lesen eines <TT>float</TT> -Werts von der Tastatur. 
+     *
+     * @return                            eingegebener <TT>float</TT> -Wert 
+     * @exception  NumberFormatException  falls kein gueltiger <TT>float 
+     *      </TT>-Wert eingegeben wird 
+     */
+    public static float readfloat() throws NumberFormatException
+    {
+        String s = readString();
+        float n = Float.valueOf(s).floatValue();
+        return n;
+    }
+
+
+    /**
+     *  Lesen eines <TT>double</TT> -Werts von der Tastatur. 
+     *
+     * @return                            eingegebener <TT>double</TT> -Wert 
+     * @exception  NumberFormatException  falls kein gueltiger <TT>double 
+     *      </TT>-Wert eingegeben wird 
+     */
+    public static double readdouble() throws NumberFormatException
+    {
+        String s = readString();
+        double n = Double.valueOf(s).doubleValue();
+        return n;
+    }
+}
+
---
+++ java-practical-11/src/RateSpiel/RateSpielStrg.java
@@ -0,0 +1,53 @@
+package RateSpiel;
+
+import javafx.application.Platform;
+import javafx.event.*;
+
+public class RateSpielStrg {
+	private Mitspieler mitspieler;
+	private RateSpielGUI gui;
+	private int zaehler;
+	
+	public RateSpielStrg(RateSpielGUI rateSpielGUI) {
+		mitspieler = new Mitspieler();
+		gui = rateSpielGUI;
+		gui.getVersuchKnopf().addEventHandler(ActionEvent.ACTION, e -> versuch());
+		gui.getNeuOpt().addEventHandler(ActionEvent.ACTION, e -> neuesSpiel());
+		gui.getEndeOpt().addEventHandler(ActionEvent.ACTION, e -> endeSpiel());
+		zaehler = 0;
+	}
+
+	/**
+	 * Methode, die nach einem Button-Click ausgef&uuml;hrt wird und den aktuellen Wert pr&uuml;ft.
+	 */
+	
+	public void versuch() {
+		zaehler++;					// + 1 Versuch
+		gui.ausgabeNachricht(zaehler + ". Versuch: " + mitspieler.bewertung(gui.holeVersuch()));
+		
+		if (mitspieler.bewertung(gui.holeVersuch()).startsWith("Erraten")) {	// Wenn die Zahl erraten wurde ...
+			gui.setEnabled(false);												// ... deaktiviere Button und Textfeld
+		}
+	}
+
+	/**
+	 * Methode, die ein neues Spiel beginnt.
+	 * Wird mit dem "Neues Spiel"-MenuItem ausgel&ouml;st.
+	 */
+	
+	public void neuesSpiel() {
+		mitspieler = new Mitspieler();												// neue Zufallszahl
+		gui.ausgabeNachricht("Kannst du eine Zahl zwischen 0 und 100 erraten?");	// neue Nachricht setzen
+		gui.setEnabled(true);														// Button und Textfeld aktivieren
+		zaehler = 0;																// Versuche auf 0 setzen
+	}
+
+	/**
+	 * Methode zum Beenden des Spiels.
+	 * Wird mit dem "Beenden"-MenuItem ausgel&ouml;st.
+	 */
+	
+	public void endeSpiel() {
+		Platform.exit();
+	}
+}
